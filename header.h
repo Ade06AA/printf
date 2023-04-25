d77bd45184932b9a98abf21c277578848cb1e14e
0ade06 <abdurrahman.adenowo06@gmail.com>
2023-04-25 21:28:29 +0000

    added some discription

--- header.h
+++ header.h
@@ -7,6 +7,6 @@
 /*functions pothotype*/
 int _putchar(char);
 int _printf(const char *, ...);
-int _print_s(char *);
+int _print_s(char *);/* the function prints the required string of an arg and return the lengt of the string printed, to be used when in a loop*/
 
 #endif
