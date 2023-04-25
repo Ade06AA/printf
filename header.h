a6f6669a235275365ea9148eff45ee82f7d175a4
0ade06 <abdurrahman.adenowo06@gmail.com>
2023-04-25 20:46:25 +0000

    made an header file wich also contains the standard hader files

---
+++ header.h
@@ -0,0 +1,12 @@
+#ifndef H_FILE
+#define H_file
+/*standard header file*/
+#include <stddef.h>
+#include <unistd.h>
+
+/*functions pothotype*/
+int _putchar(char);
+int _printf(const char *, ...);
+int _print_s(char *);
+
+#endif
