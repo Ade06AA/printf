83eadbc67c099553e458bd40fb26e98a3c5db4d9
0ade06 <abdurrahman.adenowo06@gmail.com>
2023-04-27 14:42:58 +0000

    updated print.c and main,.h

--- main.h
+++ main.h
@@ -6,9 +6,16 @@
 #include <stdarg.h>
 
 /*functions pothotype*/
+int fswitch(char, int, int);
+int _ui(char, int, unsigned int);
 int _putchar(char);
 int _printf(const char *, ...);
 int _print_s(char *);
 int _print_i(int);
 int _print_d(int);
+int _print_u(unsigned int);
+int _print_o(unsigned int);
+int _print_b(int);
+int _print_X(unsigned int);
+int _print_x(unsigned int);
 #endif
