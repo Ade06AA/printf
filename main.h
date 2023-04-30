f8d68c3c8d46c8028c9459ddba59df5977fbd1b1
0ade06 <abdurrahman.adenowo06@gmail.com>
2023-04-30 05:46:47 +0000

    finishing

--- main.h
+++ main.h
@@ -11,6 +11,7 @@ int _ui(char, int, unsigned int);
 int _putchar(char);
 int _printf(const char *, ...);
 int _print_s(char *);
+int _default(const char *);
 int _print_i(int);
 int _print_d(int);
 int _print_u(unsigned int);
