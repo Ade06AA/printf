b9e2c8e57764b012b138855351925c99b7166120
0ade06 <abdurrahman.adenowo06@gmail.com>
2023-04-30 06:00:41 +0000

    corrected the %s specifier fun

--- _print_s.c
+++ _print_s.c
@@ -8,6 +8,16 @@ int _print_s(char *s)
 {
 	int i;
 
+	if (s == NULL)
+	{
+		_putchar('(');
+		_putchar('n');
+		_putchar('i');
+		_putchar('l');
+		_putchar('l');
+		_putchar(')');
+		return (6);
+	}
 	for (i = 0; s[i]; i++)
 	{
 		_putchar(s[i]);
